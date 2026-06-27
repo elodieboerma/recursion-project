bdcfb58ee1d38fbc050ae61eeea197527cbacc9a
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-27 16:11:13 -0500

    comment requirements/instructions for fibonacci file

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,1 +1,9 @@
+function fibs(n) {
+    //return an array containing n number of numbers in the fibonacci sequence
+}
 
+function fibsRec(n) {
+    //solve the same problem recursively
+}
+
+// test both by passing in various lengths as arguments

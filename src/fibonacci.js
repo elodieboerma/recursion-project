c7fd2e4ec8597d516a8a7dfb606e61249951c100
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-27 18:25:12 -0500

    work on fibsRec()

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,9 +1,51 @@
 function fibs(n) {
-    //return an array containing n number of numbers in the fibonacci sequence
+    let arr = [0];
+    if (n === 0) {
+        return arr;
+    } else if (n === 1) {
+        arr.push(1);
+        return arr;
+    }
+
+    let num0 = 0;
+    let num1 = 1;
+    arr.push(1);
+
+    for (let i = 2; i < n; i++) {
+        let num = num0 + num1;
+        num0 = num1;
+        num1 = num;
+        arr.push(num);
+    }
+
+    return arr;
 }
+
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(9));
+
+
 
 function fibsRec(n) {
-    //solve the same problem recursively
+    console.log("This was printed recursively.");
+
+    let arr = [0,1];
+    let num0 = 0;
+    let num1 = 1;
+    let curNum;
+
+    if (n === 0) {
+        
+        return;
+    }
+
+    curNum = (fibsRec(n - 1) + fibsRec(n - 2));
+    console.log(curNum);
+    arr.push(curNum);
+    return arr;
 }
 
-// test both by passing in various lengths as arguments
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(9));
